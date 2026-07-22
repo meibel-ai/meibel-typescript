3d967728ab7ae7ff53c0b6227bda3c3afff4bcf0
kiradeva <kiran@meibel.ai>
2026-07-22 13:21:29 -0400

    Release v2.0.6

--- src/client.ts
+++ src/client.ts
@@ -57,7 +57,7 @@ export class MeibelClient {
       ...options,
       baseUrl,
       headers,
-      _sdkUserAgent: "meibel-typescript/2.0.5",
+      _sdkUserAgent: "meibel-typescript/2.0.6",
     });
 
     this.agents = new AgentsResource(this.http);
